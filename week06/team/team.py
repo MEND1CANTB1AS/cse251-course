a239e174c3180cc346c583ab40c0369f579139f3
MEND1CANTB1AS <74216076+MEND1CANTB1AS@users.noreply.github.com>
2021-02-10 19:15:54 -0700

    y

--- week06/team/team.py
+++ week06/team/team.py
@@ -24,7 +24,7 @@ import os, sys
 sys.path.append('../../code')
 from cse251 import *
 
-def sender():
+def sender(conn, count, filename1):
     """ function to send messages to other end of pipe """
     '''
     open the file
@@ -32,17 +32,28 @@ def sender():
     Note: you must break each line in the file into words and
           send those words through the pipe
     '''
-    pass
 
+    with open(filename1, 'r') as f:
+        for line in f:
+            conn.send(line)
+            count.value += 1
 
-def receiver():
+    conn.send(None)
+
+def receiver(conn, filename):
     """ function to print the messages received from other end of pipe """
     ''' 
     open the file for writing
     receive all content through the shared pipe and write to the file
     Keep track of the number of items sent over the pipe
     '''
-    pass
+    with open(filename, 'w') as f:
+        while True:
+            data = conn.recv()
+            if (data is None):
+                break
+
+            f.write(data)
 
 
 def are_files_same(filename1, filename2):
@@ -52,22 +63,31 @@ def are_files_same(filename1, filename2):
 
 def copy_file(log, filename1, filename2):
     # TODO create a pipe 
+    parent_pipe, child_pipe = mp.Pipe()
     
     # TODO create variable to count items sent over the pipe
+    num = Value('i', 0)
 
     # TODO create processes 
+    processes = []
+    processes.append(mp.Process(target=sender, args=(parent_pipe, num, filename1)))
+    processes.append(mp.Process(target=receiver, args=(child_pipe, filename2)))
 
     log.start_timer()
     start_time = log.get_time()
 
     # TODO start processes 
-    
+    for p in processes:
+        p.start()
+
     # TODO wait for processes to finish
+    for p in processes:
+        p.join()
 
     stop_time = log.get_time()
 
-    log.stop_timer(f'Total time to transfer content = {PUT YOUR COUNT VARIABLE HERE}: ')
-    log.write(f'items / second = {PUT YOUR COUNT VARIABLE HERE / (stop_time - start_time)}')
+    log.stop_timer(f'Total time to transfer content = {num.value}: ')
+    log.write(f'items / second = {num.value / (stop_time - start_time)}')
 
     if are_files_same(filename1, filename2):
         log.write(f'{filename1} - Files are the same')
